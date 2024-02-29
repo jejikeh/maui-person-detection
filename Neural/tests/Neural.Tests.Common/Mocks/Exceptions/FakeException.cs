809e0a9b490e4ed90917cc4a69f943b0545f5f09
jejikeh <jejikeh@gmail.com>
2024-02-29 13:59:22 +0300

    feature: Neural Core, Common tests

---
+++ Neural/tests/Neural.Tests.Common/Mocks/Exceptions/FakeException.cs
@@ -0,0 +1,3 @@
+namespace Neural.Tests.Common.Mocks.Exceptions;
+
+public class FakeException(string _message) : Exception(_message);
---
+++ Neural/tests/Neural.Tests.Common/Mocks/Models/Yolo5/Yolo5ModelStringToStringMock.cs
@@ -0,0 +1,5 @@
+using Neural.Tests.Common.Mocks.Models.Tasks;
+
+namespace Neural.Tests.Common.Mocks.Models.Yolo5;
+
+public class Yolo5ModelStringToStringMock : Yolo5Model<StringToStringTaskMock>;
