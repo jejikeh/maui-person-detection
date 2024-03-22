022c0cafe83a25ed044773d4d6e7699f2d5f65b4
jejikeh <jejikeh@gmail.com>
2024-03-22 22:19:37 +0300

    misc: Expose minio console, delete empty file. Create bucket in minio if it dosent exist

--- Backend/PersonDetection.Backend.Infrastructure/Common/Options/ImageBucketOptions.cs
+++ Backend/PersonDetection.Backend.Infrastructure/Common/Options/ImageBucketOptions.cs
@@ -5,4 +5,5 @@ public class ImageBucketOptions
     public string Endpoint { get; set; } = string.Empty;
     public string AccessKey { get; set; } = string.Empty;
     public string SecretKey { get; set; } = string.Empty;
+    public string BucketName { get; set; } = string.Empty;
 }
--- Backend/PersonDetection.Backend.Infrastructure/Services/Implementations/ImageBucketService.cs
+++ Backend/PersonDetection.Backend.Infrastructure/Services/Implementations/ImageBucketService.cs
@@ -1,25 +1,27 @@
+using Microsoft.Extensions.Options;
 using Minio;
 using Minio.DataModel.Args;
+using PersonDetection.Backend.Infrastructure.Common.Options;
 
 namespace PersonDetection.Backend.Infrastructure.Services.Implementations;
 
-public class ImageBucketService(IMinioClient _minioClient) : IImageBucketService
+public class ImageBucketService(IMinioClient _minioClient, IOptions<ImageBucketOptions> options) : IImageBucketService
 {
-    private const string _bucketName = "person-detection";
-    
     public async Task SavePhotoAsync(string photoName, string photo)
     {
+        await CreateBucketIfNotExistsAsync();
+
         var bytes = Convert.FromBase64String(photo);
-        
+
         using var photoMemoryStream = new MemoryStream(bytes);
 
         var putObjectArgs = new PutObjectArgs()
-            .WithBucket(_bucketName)
+            .WithBucket(options.Value.BucketName)
             .WithStreamData(photoMemoryStream)
             .WithObject(photoName)
             .WithContentType("image/png")
             .WithObjectSize(photoMemoryStream.Length);
-        
+
         await _minioClient.PutObjectAsync(putObjectArgs);
     }
 
@@ -28,19 +30,36 @@ public class ImageBucketService(IMinioClient _minioClient) : IImageBucketService
         var photo = string.Empty;
 
         var getObjectArgs = new GetObjectArgs()
-            .WithBucket(_bucketName)
+            .WithBucket(options.Value.BucketName)
             .WithObject(photoName)
             .WithCallbackStream((stream) =>
             {
                 using var memoryStream = new MemoryStream();
-                
+
                 stream.CopyTo(memoryStream);
-                
+
                 photo = Convert.ToBase64String(memoryStream.ToArray());
             });
-        
+
         await _minioClient.GetObjectAsync(getObjectArgs);
-        
+
         return photo;
+    }
+
+    private async Task CreateBucketIfNotExistsAsync()
+    {
+        var exists = await _minioClient.BucketExistsAsync(
+            new BucketExistsArgs()
+            .WithBucket(options.Value.BucketName));
+
+        if (exists)
+        {
+            return;
+        }
+
+        var makeBucketArgs = new MakeBucketArgs()
+            .WithBucket(options.Value.BucketName);
+
+        await _minioClient.MakeBucketAsync(makeBucketArgs);
     }
 }
